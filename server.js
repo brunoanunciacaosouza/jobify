f8161f755791f032b4799d97cc60cf847f249944
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-05 20:32:06 -0300

    auth controller and routes

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 import express from "express";
-import notFoundMiddleware from "./middleware/not-found.js";
-import dotenv from "dotenv";
 import connectDB from "./db/connect.js";
+import notFoundMiddleware from "./middleware/not-found.js";
+import authRouter from "./routes/authRoutes.js";
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("<h1>Welcome!</h1>");
 });
+
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 
