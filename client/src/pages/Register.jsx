fe59374f1cdba228efdc55d9fe37bcc17228d88c
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-05 17:36:50 -0300

    page setup

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { Logo } from "../components";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = ({ target }) => {
+    console.log(target);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={handleSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            value={values.name}
+            onChange={handleChange}
+            name="name"
+            className="form-input"
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
