c4db6d1c0e59217ddcd5ba9a662aa47af67dd427
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-05 14:58:43 -0300

    landing page - structure

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -3,9 +3,25 @@ import main from "../assets/images/main.svg";
 
 const Landing = () => {
   return (
-    <>
-      <h4>Landing Page</h4>
-    </>
+    <main>
+      <nav>
+        <img src={logo} alt="Jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app.
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+            commodi sint? Dolor, saepe! Mollitia omnis consequuntur fugiat
+            commodi nobis, incidunt porro facere quidem ipsa officia!
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
   );
 };
 
