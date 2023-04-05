a8976c875d70f66dcbc86654b8565d624f33806e
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-05 19:07:38 -0300

    errorHandler middleware

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import notFoundMiddleware from "./middleware/not-found.js";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome!</h1>");
 });
+
+app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 5000;
 
