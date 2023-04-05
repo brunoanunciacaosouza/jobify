a4cf152eef18dd74f2db0775a6d873e3f1b0afb8
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-05 15:07:06 -0300

    wrapper styled components

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,27 +1,30 @@
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
 
 const Landing = () => {
   return (
-    <main>
-      <nav>
-        <img src={logo} alt="Jobify" className="logo" />
-      </nav>
-      <div className="container page">
-        <div className="info">
-          <h1>
-            job <span>tracking</span> app.
-          </h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
-            commodi sint? Dolor, saepe! Mollitia omnis consequuntur fugiat
-            commodi nobis, incidunt porro facere quidem ipsa officia!
-          </p>
-          <button className="btn btn-hero">Login/Register</button>
+    <Wrapper>
+      <main>
+        <nav>
+          <img src={logo} alt="Jobify" className="logo" />
+        </nav>
+        <div className="container page">
+          <div className="info">
+            <h1>
+              job <span>tracking</span> app.
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+              commodi sint? Dolor, saepe! Mollitia omnis consequuntur fugiat
+              commodi nobis, incidunt porro facere quidem ipsa officia!
+            </p>
+            <button className="btn btn-hero">Login/Register</button>
+          </div>
+          <img src={main} alt="job hunt" className="img main-img" />
         </div>
-        <img src={main} alt="job hunt" className="img main-img" />
-      </div>
-    </main>
+      </main>
+    </Wrapper>
   );
 };
 
