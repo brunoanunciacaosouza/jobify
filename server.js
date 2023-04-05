8c88b15592ba0c4ca042d71255663dc68aaa25be
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-05 19:02:36 -0300

    basic express server and nodemon

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("server running...");
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome!</h1>");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+});
