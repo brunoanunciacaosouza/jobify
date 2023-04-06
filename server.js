8763f3331dbb54abdbaaa86b8e1f42dc806bb475
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-06 00:42:42 -0300

    basic register controller

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGO_URI).then(() =>
+      console.log("connected db")
+    );
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
