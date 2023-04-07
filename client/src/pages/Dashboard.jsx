5a903f94dfcbb8070d740368d86955e85c539fc3
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-07 16:42:37 -0300

    fetch error

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 const Dashboard = () => {
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:5000/");
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return <div>Dashboard</div>;
 };
 
