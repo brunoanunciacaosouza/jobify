5ff31086be445c5c6ab1f89f652364dcfe75d107
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-07 17:07:21 -0300

    proxy setup

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import "express-async-errors";
-import cors from "cors";
 import express from "express";
 import connectDB from "./db/connect.js";
 import notFoundMiddleware from "./middleware/not-found.js";
@@ -11,10 +10,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/api/v1", (req, res) => {
   res.json({ msg: "Welcome" });
 });
 
