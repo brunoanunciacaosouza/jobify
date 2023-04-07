eb9a5e873042e108b686ae1f42d02082c21102ed
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-07 14:19:57 -0300

    unique  field error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -13,6 +13,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
       .join(", ");
   }
 
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} Field has to be unique`;
+  }
+
   res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
