822a98f097284c6a204a190c829f7da0e4d641a9
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-08 15:42:17 -0300

    structure

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import Wrapper from "../assets/wrappers/Navbar";
+
+const Navbar = () => {
+  return (
+    <Wrapper>
+      <h3>Navbar</h3>
+    </Wrapper>
+  );
+};
+
+export default Navbar;
---
+++ client/src/components/SmallSidebar.jsx
@@ -0,0 +1,11 @@
+import Wrapper from "../assets/wrappers/SmallSidebar";
+
+const SmallSidebar = () => {
+  return (
+    <Wrapper>
+      <h3>Smallsidebar</h3>
+    </Wrapper>
+  );
+};
+
+export default SmallSidebar;
