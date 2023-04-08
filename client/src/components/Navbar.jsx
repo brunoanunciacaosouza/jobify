836d2e127fc0ebf3813ec1c39df9f360111d7189
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-08 15:45:45 -0300

    react icons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import Wrapper from "../assets/wrappers/Navbar";
+import { FaHome } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h3>Navbar</h3>
+      <h4>Navbar</h4>
+      <FaHome />
     </Wrapper>
   );
 };
