27ae4eda332d45350b4f2fe2dd5ff7f10481f29b
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-08 20:29:03 -0300

    navLinks

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,25 +1,44 @@
 import { NavLink } from "react-router-dom";
 import { FaTimes } from "react-icons/fa";
-import Links from "../utils/Links";
+import links from "../utils/Links";
 import Logo from "../components/Logo";
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { useAppContext } from "../context/appContext";
 
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <div className="sidebar-container show-sidebar">
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
         <div className="content">
-          <button
-            type="button"
-            className="close-btn"
-            onClick={() => console.log("toggle sidebar")}
-          >
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
             <FaTimes />
           </button>
           <header>
             <Logo />
           </header>
-          <div className="nav-links"></div>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, id, icon } = link;
+              return (
+                <NavLink
+                  to={path}
+                  key={id}
+                  onClick={toggleSidebar}
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
         </div>
       </div>
     </Wrapper>
