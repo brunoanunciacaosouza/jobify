89e90286934b2332751da7cfd264302c365f21eb
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-08 19:52:28 -0300

    toggle

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,9 +1,27 @@
+import { NavLink } from "react-router-dom";
+import { FaTimes } from "react-icons/fa";
+import Links from "../utils/Links";
+import Logo from "../components/Logo";
 import Wrapper from "../assets/wrappers/SmallSidebar";
 
 const SmallSidebar = () => {
   return (
     <Wrapper>
-      <h3>Smallsidebar</h3>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button
+            type="button"
+            className="close-btn"
+            onClick={() => console.log("toggle sidebar")}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links"></div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
