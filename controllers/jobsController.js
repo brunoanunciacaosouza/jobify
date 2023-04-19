ebfba5864a730ef7b0635b16456d538a9d10c09e
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-18 23:14:21 -0300

    delete job controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -53,12 +53,13 @@ const deleteJob = async (req, res) => {
   const job = await Job.findOne({ _id: jobId });
 
   if (!job) {
-    throw new NotFoundError(`No job with id : ${jobId}`);
+    throw new NotFoundError(`No job with id :${jobId}`);
   }
 
   checkPermissions(req.user, job.createdBy);
 
-  await job.remove();
+  await Job.findOneAndRemove({ _id: jobId });
+
   res.status(StatusCodes.OK).json({ msg: 'Success! Job removed' });
 };
 
