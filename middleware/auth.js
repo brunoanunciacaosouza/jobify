0f1bf56286e051db274cc05d48ec5cbaf1c9cdc9
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-21 17:58:38 -0300

    restrict access

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,22 @@
-import jwt from "jsonwebtoken";
-import { UnAuthenticatedError } from "../errors/index.js";
+import jwt from 'jsonwebtoken';
+import { UnAuthenticatedError } from '../errors/index.js';
 
 const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer")) {
-    throw new UnAuthenticatedError("Authentication Invalid");
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
+    throw new UnAuthenticatedError('Authentication Invalid');
   }
 
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(' ')[1];
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = { userId: payload.userId };
+    const testUser = payload.userId === '6442bf53f94b1cbc05e2dbc8';
+    req.user = { userId: payload.userId, testUser };
     next();
   } catch (error) {
-    throw new UnAuthenticatedError("Authentication Invalid");
+    throw new UnAuthenticatedError('Authentication Invalid');
   }
 };
 
