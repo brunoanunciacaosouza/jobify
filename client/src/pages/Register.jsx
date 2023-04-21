09ec9c565cd07282108d5aef9d88824f5c2f6f81
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-21 14:11:10 -0300

    login test user

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Wrapper from "../assets/wrappers/RegisterPage";
-import { Logo, FormRow, Alert } from "../components";
-import { useAppContext } from "../context/appContext";
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterPage';
+import { Logo, FormRow, Alert } from '../components';
+import { useAppContext } from '../context/appContext';
 
 const initialState = {
-  name: "",
-  email: "",
-  password: "",
+  name: '',
+  email: '',
+  password: '',
   isMember: true,
 };
 
@@ -39,14 +39,14 @@ const Register = () => {
     if (isMember) {
       setupUser({
         currentUser,
-        endPoint: "login",
-        alertText: "Login Successful! Redirecting...",
+        endPoint: 'login',
+        alertText: 'Login Successful! Redirecting...',
       });
     } else {
       setupUser({
         currentUser,
-        endPoint: "register",
-        alertText: "User Created! Redirecting...",
+        endPoint: 'register',
+        alertText: 'User Created! Redirecting...',
       });
     }
   };
@@ -54,47 +54,64 @@ const Register = () => {
   useEffect(() => {
     if (user) {
       setTimeout(() => {
-        navigate("/stats");
+        navigate('/stats');
       }, 3000);
     }
   }, [user, navigate]);
 
   return (
-    <Wrapper className="full-page">
-      <form className="form" onSubmit={handleSubmit}>
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={handleSubmit}>
         <Logo />
-        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
         {showAlert && <Alert />}
         {!values.isMember && (
           <FormRow
-            labelText="name"
-            name="name"
-            type="text"
+            labelText='name'
+            name='name'
+            type='text'
             value={values.name}
             handleChange={handleChange}
           />
         )}
         <FormRow
-          labelText="email"
-          name="email"
-          type="email"
+          labelText='email'
+          name='email'
+          type='email'
           value={values.email}
           handleChange={handleChange}
         />
         <FormRow
-          labelText="password"
-          name="password"
-          type="password"
+          labelText='password'
+          name='password'
+          type='password'
           value={values.password}
           handleChange={handleChange}
         />
-        <button type="submit" className="btn btn-block" disabled={isLoading}>
+        <button type='submit' className='btn btn-block' disabled={isLoading}>
           Submit
         </button>
+        <button
+          type='button'
+          className='btn btn-block btn-hipster'
+          disabled={isLoading}
+          onClick={() => {
+            setupUser({
+              currentUser: {
+                email: 'testUser@test.com',
+                password: '123456789',
+              },
+              endPoint: 'login',
+              alertText: 'Login Successful! Redirecting...',
+            });
+          }}
+        >
+          {isLoading ? 'loading...' : 'demo app'}
+        </button>
         <p>
-          {values.isMember ? "Not a member yet?" : "Already a member?"}
-          <button type="button" onClick={toggleMember} className="member-btn">
-            {values.isMember ? "Register" : "Login"}
+          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+          <button type='button' onClick={toggleMember} className='member-btn'>
+            {values.isMember ? 'Register' : 'Login'}
           </button>
         </p>
       </form>
