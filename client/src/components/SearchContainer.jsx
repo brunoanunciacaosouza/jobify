c3448a07f912e23d846062063e85a7aeb9507d23
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-20 22:17:57 -0300

    initial setup

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import { FormRow, FormRowSelect } from '.';
+import { useAppContext } from '../context/appContext';
+import Wrapper from '../assets/wrappers/SearchContainer';
 
 const SearchContainer = () => {
-  return <div>SearchContainer</div>;
+  const {
+    isLoading,
+    search,
+    searchStatus,
+    searchType,
+    sort,
+    sortOptions,
+    statusOptions,
+    jobTypeOptions,
+    handleChange,
+    clearFilters,
+  } = useAppContext();
+
+  const handleSearch = (e) => {
+    if (isLoading) return;
+    handleChange({ name: e.target.name, value: e.target.value });
+  };
+
+  return (
+    <Wrapper>
+      <form className='form'>
+        <h4>search form</h4>
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='search'
+            value={search}
+            handleChange={handleSearch}
+          />
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default SearchContainer;
