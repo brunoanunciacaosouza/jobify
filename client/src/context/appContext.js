6f8fec7ffbc7d40c55e348a59add8a8c0dd19cf0
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-20 22:27:49 -0300

    clear filters

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -26,6 +26,7 @@ import {
   EDIT_JOB_ERROR,
   SHOW_STATS_BEGIN,
   SHOW_STATS_SUCCESS,
+  CLEAR_FILTERS,
 } from './actions';
 
 const user = localStorage.getItem('user');
@@ -285,7 +286,7 @@ const AppProvider = ({ children }) => {
   };
 
   const clearFilters = () => {
-    console.log('clear filters');
+    dispatch({ type: CLEAR_FILTERS });
   };
 
   return (
